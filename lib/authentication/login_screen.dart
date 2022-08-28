8861b1a5c70aa8fbaa9dc5a7d029a6c16dc6a40d
Sudesh <sudeshthakuradstudio@gmail.com>
2022-08-28 20:32:10 +0530

    1st push

---
+++ lib/authentication/login_screen.dart
@@ -0,0 +1,178 @@
+import 'package:drivers_app/authentication/signup_screen.dart';
+import 'package:drivers_app/global/global.dart';
+import 'package:drivers_app/splashScreen/splash_screen.dart';
+import 'package:drivers_app/widgets/progress_dialog.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class LoginScreen extends StatefulWidget {
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+
+  validationForm() {
+    if (!emailTextEditingController.text.contains("@") ||
+        emailTextEditingController.text.isEmpty) {
+      Fluttertoast.showToast(msg: "Email address is not valid.");
+    } else if (passwordTextEditingController.text.isEmpty) {
+      Fluttertoast.showToast(msg: "Password is required");
+    } else {
+      loginDriverNow();
+    }
+  }
+
+  loginDriverNow() async {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext c) {
+          return ProgressDialog(message: "Progressing, Please wait...",);
+        }
+    );
+
+    final User? firebaseUser = (
+        await fAuth.signInWithEmailAndPassword(
+          email: emailTextEditingController.text.trim(),
+          password: passwordTextEditingController.text.trim(),
+        ).catchError((msg){
+          Navigator.pop(context);
+          Fluttertoast.showToast(msg: "Error: "+msg.toString());
+        })
+    ).user;
+
+    if(firebaseUser != null){
+      currentFirebaseUser = firebaseUser;
+
+      Fluttertoast.showToast(msg: "Login Successful.");
+      Navigator.push(context, MaterialPageRoute(builder: (c)=> MySplashScreen()));
+
+    }else{
+      Navigator.pop(context);
+      Fluttertoast.showToast(msg: "Error occurred during Login.");
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 30,),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Image.asset("images/logo1.png"),
+              ),
+
+              const SizedBox(height: 10,),
+              const Text(
+                'Login as a Driver',
+                style: TextStyle(
+                    fontSize: 26,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+
+              //email
+              TextField(
+                controller: emailTextEditingController,
+                keyboardType: TextInputType.emailAddress,
+                style: const TextStyle(
+                  color: Colors.grey,
+                ),
+                decoration: const InputDecoration(
+                  labelText: "Email",
+                  hintText: "Email",
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey)
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey)
+                  ),
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 10,
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+
+
+              //password
+              TextField(
+                controller: passwordTextEditingController,
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                style: const TextStyle(
+                  color: Colors.grey,
+                ),
+                decoration: const InputDecoration(
+                  labelText: "Password",
+                  hintText: "Passowrd",
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey)
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey)
+                  ),
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 10,
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+
+
+              const SizedBox(height: 20,),
+              ElevatedButton(
+                onPressed: (){
+                  validationForm();
+                },
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.lightGreenAccent
+                ),
+                child: const Text(
+                  "Login",
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+
+              TextButton(
+              child: const Text(
+              "Do not have an account? SingUp Here ",
+                  style: TextStyle(color: Colors.grey),
+                ),
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (c)=> SignupScreen()));
+                },
+              )
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
